efb7c491ffc142abcba80afa60f86f2f60b9c658
Ankitchoudhary28 <91711168+Ankitchoudhary28@users.noreply.github.com>
2021-10-01 01:04:09 -0700

    Update lambda_funtion.py
    Change in prompt time - 4s less

--- Hello World/python/lambda_funtion.py
+++ Hello World/python/lambda_funtion.py
@@ -25,7 +25,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
+        speak_output = "Welcome User, you can say Hello or Help. Which would you like to try?"
 
         return (
             handler_input.response_builder
